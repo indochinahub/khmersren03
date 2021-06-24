e6c888967b9f346e8e18c225c4107e5368082e4d
indochinahub <indochinahub@gmail.com>
2021-06-24 20:46:44 +0700

    Add first template

--- app/Views/Main/index.php
+++ app/Views/Main/index.php
@@ -11,7 +11,8 @@
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <!-- Our Custom CSS -->
-    <link rel="stylesheet" href="style3.css">
+    <link rel="stylesheet" href="<?php echo base_url(["include","css","main_style.css"]);?>">
+    
     <!-- Scrollbar Custom CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
