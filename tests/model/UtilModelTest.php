f4a7a6e6a7a4e9c55e75435c2b7decbd5b2fd4ff
indochinahub <indochinahub@gmail.com>
2021-06-24 18:14:38 +0700

    commit

--- tests/model/UtilModelTest.php
+++ tests/model/UtilModelTest.php
@@ -7,21 +7,22 @@ use CodeIgniter\Test\CIUnitTestCase;
 class UtilModelTest extends CIUnitTestCase
 {
 
+    var $util_model;
+
     public function __construct(){
         parent::__construct();
+        $this->util_model =
+        
         
     }
 
     public function test_get_class_from_fullname()
     {
         $util_model = new UtilModel();
-        $result = $util_model->get_class_from_fullname("\Apps\Test\MyClass");
+        $result1 = $util_model->get_class_from_fullname("\Apps\Test\MyClass");
+        $result2 = $util_model->get_class_from_fullname("\Apps\Test\MyClass");
         
-        echo "*******************\n";
-        echo " test test test \n";
-        echo "*******************\n";
-        
-        $result             =   [ $result ];
+        $result             =   [ $result1, $result2 ];
         $expectedResult     =   [ "MyClass" ];
 
         $this->assertSame($result,$expectedResult);
