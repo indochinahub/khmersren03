ed8b89dcc7e1a3f99a3e303a84a91372b5387c93
indochinahub <indochinahub@gmail.com>
2021-07-08 10:58:30 +0700

    เปลี่ยนชื่อ BaseModel เป็น MyModel

--- app/Models/MyModel.php
+++ app/Models/MyModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class BaseModel extends Model
+class MyModel extends Model
 {
     protected $table      ;
     protected $primaryKey ;
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UserModel extends BaseModel
+class UserModel extends MyModel
 {
 
     public function __construct(){
