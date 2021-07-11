0c5a4638130352ccee7366d9f510b6f98d94c44e
indochinahub <indochinahub@gmail.com>
2021-07-11 17:37:59 +0700

    กำหนดให้มีการแสดง noticebox เฉพาะ localhost

--- app/Views/section/070noticebox.php
+++ app/Views/section/070noticebox.php
@@ -1,5 +1,8 @@
+<?php  if( SHOW_NOTICE === true){ ?>
 
-<div style="background-color:yellow;padding:10px">
-    Controller :: <?php echo $controller_name;?><br>
-    Uid :: <?php echo $uid;?><br>
-</div>            
+    <div style="background-color:yellow;padding:10px">
+        Controller :: <?php echo $controller_name;?><br>
+        Uid :: <?php echo $uid;?><br>
+    </div>            
+    
+<?php } ?>
