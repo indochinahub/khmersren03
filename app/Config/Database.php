78d403841f2c84700d82625074a2aa6aa7dd6464
indochinahub <indochinahub@gmail.com>
2021-07-11 17:38:33 +0700

    เริ่มใช้แฟ้ม .env

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -33,8 +33,8 @@ class Database extends Config
 	public $default = [
 		'DSN'      => '',
 		'hostname' => '127.0.0.1',
-		'username' => 'root',
-		'password' => '',
+		'username' => 'wittayan_khmersren',
+		'password' => 'khmersren',
 		'database' => 'wittayan_khmersren',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
