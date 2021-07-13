ad7d48e5c08a45d335d12ea14219fb0ab7e82fd4
indochinahub <indochinahub@gmail.com>
2021-07-13 12:09:38 +0700

    ปรับปรุงให้แสดง course ทั้งหมด

--- app/Controllers/Course.php
+++ app/Controllers/Course.php
@@ -2,14 +2,73 @@
 
 namespace App\Controllers;
 
+use App\Models\CourseTypeModel;
+use App\Models\CourseModel;
+use App\Models\UtilModel;
 class Course extends MyController
 {
 
+	public function showAll() {
 
-	public function showAll()	{
+        $coursetype_model = new CourseTypeModel();
+        $course_model = new CourseModel();
+        $util_model = new UtilModel();
+
+        $arr_course_origin = $course_model->get_all_row();
+        $arr_course = [];
+        foreach( $arr_course_origin as $course){
+            $course->icon_url = $course_model->get_icon_url($course);
+            array_push($arr_course,$course);
+        }
+
+        $arr_coursetype = $coursetype_model->get_all_row();
+
+        $data["arr_coursetype"] = [];
+        foreach( $arr_coursetype as $coursetype){
+
+            $arr_course_of_coursetype = $this->util_model->get_object_from_arr_object_that_match_property_condition(
+                            $origin_arr_object = $arr_course, 
+                            $property_name = "id_coursetype", 
+                            $text_to_compare = $coursetype->coursetype_id, 
+                            $operator = "==");
+
+            if( count($arr_course_of_coursetype) > 0 ){
+
+                $arr_course_as_row = $this->util_model->separate_array_to_pair_value($arr_course_of_coursetype);
+
+                /*
+                echo "coursetype id :: $coursetype->coursetype_id <br>";
+                echo "num of course :: ".count($arr_course_of_coursetype)."<br>";
+                echo "<hr>";
+                var_dump($arr_course_as_row);
+                echo "<hr>";
+                */
+
+                $coursetype->arr_course_as_row = $arr_course_as_row;
+                array_push($data["arr_coursetype"],$coursetype);
+            }
+    
+            
+
+        }
+
+
+        $data["page_title"] = 	"วิชาทั้งหมด";
+        $data["page_link"] 	= 	[	"Home",
+                                    base_url()
+                                ];	        
+        $this->_view("showAll",$data);
+
+
 		
 
 	}
+
+
+    public function show($course_id){
+        echo "Course id : $course_id";
+
+    }
 
 
 }
---
+++ app/Views/Course/showAll.php
@@ -0,0 +1,59 @@
+<div class="card-info">
+
+    <?php foreach( $arr_coursetype as $coursetype){ ?>
+        <div class="card-info card-info_header">
+            <?php echo $coursetype->coursetype_name;?>
+        </div>
+
+        <div class="card-info card-info_body">
+
+            <?php foreach( $coursetype->arr_course_as_row as $row ){ ?>
+
+                <div class="icon_row">
+                    <div class="course_icon">
+                        <div class="course_icon-thumbnail">
+                            <a href="<?php echo base_Url(["Course","show", $row[0]->course_id]);?>">
+                                <img src="<?php echo $row[0]->icon_url;?>" width="100%">
+                            </a>
+                        </div>
+                        <div class="course_icon-title" style="">
+                            <a href="<?php echo base_Url(["Course","show", $row[0]->course_id]);?>">
+                                <strong><?php echo $row[0]->course_code;?></strong><br>
+                                <?php echo $row[0]->course_name;?>
+                            </a>
+                        </div>		
+                    </div>		
+                    
+                    <?php if( $row[1] !== false ){ ?>
+                        <div class="course_icon">
+                            <div class="course_icon-thumbnail">
+                                <a href="<?php echo base_Url(["Course","show", $row[1]->course_id]);?>">
+                                    <img src="<?php echo $row[1]->icon_url;?>" width="100%">
+                                </a>
+                            </div>
+                            <div class="course_icon-title" style="">
+                                <a href="<?php echo base_Url(["Course","show", $row[1]->course_id]);?>">
+                                    <strong><?php echo $row[1]->course_code;?></strong><br>
+                                    <?php echo $row[1]->course_name;?>
+                                </a>
+                            </div>		
+                        </div>		
+                    <?php }else{ ?>
+                        <div>
+                        </div>		                        
+                        
+                    <?php } ?>
+
+                </div>
+
+            <?php } ?>                
+                
+
+            
+            
+        </div>
+    <?php } ?>
+
+
+    
+</div>
