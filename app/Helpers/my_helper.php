e5f4c94276ca8ab193464174730cae7ca53515b9
indochinahub <indochinahub@gmail.com>
2021-07-14 14:47:43 +0700

    เพิ่มความเห็นใน my_helper

--- app/Helpers/my_helper.php
+++ app/Helpers/my_helper.php
@@ -1,15 +1,13 @@
 <?php
 
-    // 
+    // return text
     function get_text_of_minute_and_hour_by_second($second){
 
         if( $second < 60){
             return $second." วินาที";
-
         }elseif( $second < 3600){
             $minute = round ($second/60 , $precision = 0 ) ;
             return $minute." นาที"; 
-            
         }else{
             $hour = round ($second/3600 , $precision = 2 ) ;
             return $hour." ชั่วโมง";
