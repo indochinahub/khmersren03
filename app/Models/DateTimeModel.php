d87a6ebfd3cb709dacab39a2bae7510e866d4a25
indochinahub <indochinahub@gmail.com>
2021-07-14 22:05:43 +0700

    Update DateTimeModel.php

--- app/Models/DateTimeModel.php
+++ app/Models/DateTimeModel.php
@@ -6,7 +6,7 @@ class DateTimeModel
 {
     // return Int
     public function get_unix_timestamp($unix_timestamp , $next_day = 0){
-        return time() + (60*60*24*$next_day);
+        return $unix_timestamp + (60*60*24*$next_day);
     }
 
     // return sql_timestamp
