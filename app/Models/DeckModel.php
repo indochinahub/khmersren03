4a7c32c7fc8b6e45f0b609e2a8cf7fcef8c3c2bc
indochinahub <indochinahub@gmail.com>
2021-07-15 21:49:22 +0700

    DeckModel:get_by_cardgroup_id() ปรับปรุง

--- app/Models/DeckModel.php
+++ app/Models/DeckModel.php
@@ -19,17 +19,8 @@ class DeckModel extends MyModel
     // return Array Of Object
     public function get_by_cardgroup_id($cardgroup_id){
 
-        $util_model = new UtilModel;
-
-        $arr_deck = $this->get_all_row();
-        
-        $arr_deck = $util_model->get_object_from_arr_object_that_match_property_condition(
-                            $origin_arr_object = $arr_deck, 
-                            $property_name = "id_cardgroup", 
-                            $text_to_compare = $cardgroup_id, 
-                            $operator = "==");
-
-        return $arr_deck;
+        $where_clause = " WHERE id_cardgroup = ".$cardgroup_id ;
+        return $this->get_where($where_clause);
     }    
 
 
