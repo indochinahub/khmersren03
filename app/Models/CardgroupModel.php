5251f96e64eba5cbd74409c200a97d4b7459698a
indochinahub <indochinahub@gmail.com>
2021-07-15 21:38:50 +0700

    CardgroupModel:get_by_course_id()

--- app/Models/CardgroupModel.php
+++ app/Models/CardgroupModel.php
@@ -16,20 +16,8 @@ class CardgroupModel extends MyModel
     // return array Of object Or blank array
     public function get_by_course_id($course_id){
 
-        $util_model = new UtilModel();
-
-        $arr_cardgroup = $this->get_all_row();
-
-        $arr_cardgroup = $util_model->get_object_from_arr_object_that_match_property_condition(
-                                    $origin_arr_object = $arr_cardgroup, 
-                                    $property_name = "id_course", 
-                                    $text_to_compare = $course_id, 
-                                    $operator = "==");
-
-        return $arr_cardgroup;
-
-
-
+        $where_clause = " WHERE id_course = ".$course_id ;
+        return $this->get_where($where_clause);
     }
 
 
