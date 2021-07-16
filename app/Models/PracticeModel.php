23c5fb63f7d6e0335af251ce8a9deb05b4b0a1de
indochinahub <indochinahub@gmail.com>
2021-07-16 08:56:42 +0700

    PracticeModel:get_by_deck_id_user_id()

--- app/Models/PracticeModel.php
+++ app/Models/PracticeModel.php
@@ -19,23 +19,9 @@ class PracticeModel extends MyModel
     public function get_by_deck_id_user_id($deck_id, $user_id){
         $util_model = new UtilModel();
 
-        $arr_practice = $this->get_all_row();
+        $where_clause = " WHERE id_deck = ".$deck_id." AND id_user = ".$user_id;
+        return $this->get_where($where_clause);
 
-        // Filter by deck_id
-        $arr_practice = $util_model->get_object_from_arr_object_that_match_property_condition(
-                            $origin_arr_object = $arr_practice, 
-                            $property_name = "id_deck", 
-                            $text_to_compare = $deck_id, 
-                            $operator = "==" );
-
-        // Filter by user_id
-        $arr_practice = $util_model->get_object_from_arr_object_that_match_property_condition(
-                            $origin_arr_object = $arr_practice, 
-                            $property_name = "id_user", 
-                            $text_to_compare = $user_id, 
-                            $operator = "==" );
-    
-        return $arr_practice;
     }
 
     // return array of object
