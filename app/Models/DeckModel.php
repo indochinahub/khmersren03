fe203ef4f617c77230f7ff87522fc436014760b6
indochinahub <indochinahub@gmail.com>
2021-07-16 08:50:26 +0700

    DeckModel : get_by_course_id() ปรับปรุงการทำงาน

--- app/Models/DeckModel.php
+++ app/Models/DeckModel.php
@@ -28,21 +28,14 @@ class DeckModel extends MyModel
     public function get_by_course_id($course_id){
 
         $cardgroup_model = new CardgroupModel;
-        $deck_model = new DeckModel;
-        $util_model = new UtilModel;
 
-        $arr_origin_deck = $deck_model->get_all_row();
         $arr_cardgroup = $cardgroup_model->get_by_course_id($course_id);
 
         $arr_deck = [];
-        foreach( $arr_cardgroup as $cardgroup ){
-            $arr_deck_of_cardgroup = $util_model->get_object_from_arr_object_that_match_property_condition(
-                                        $origin_arr_object = $arr_origin_deck , 
-                                        $property_name = "id_cardgroup", 
-                                        $text_to_compare = $cardgroup->cardgroup_id, 
-                                        $operator = "==");
-
-            $arr_deck  = array_merge($arr_deck,$arr_deck_of_cardgroup);
+        foreach($arr_cardgroup as $cardgroup){
+            $arr_deck_of_cardgroup = $this->get_by_cardgroup_id($cardgroup->cardgroup_id);
+            $arr_deck = array_merge($arr_deck, $arr_deck_of_cardgroup);
+            
         }
 
         return $arr_deck;
