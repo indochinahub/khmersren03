d67feb3f0026041b0a86fbb6afae8de8b482fe81
indochinahub <indochinahub@gmail.com>
2021-07-21 14:10:58 +0700

    DeckModel:get_average_interval() เพิ่มพร้อม unittest

--- tests/model/DeckModelTest.php
+++ tests/model/DeckModelTest.php
@@ -55,5 +55,44 @@ class DeckModelTest extends CIUnitTestCase
 
         $this->assertSame($result,$expectedResult);
     }
+
+    // return Int Or Zero
+    public function test_get_average_interval(){
+        
+        $result1 = $this->deck_model->get_average_interval(
+                                    $deck_id = 0,
+                                    $user_id = 0
+                                );
+        $result2 = $this->deck_model->get_average_interval(
+                                    $deck_id = 1,
+                                    $user_id = 1
+                                );
+
+        $result             =   [ 
+                                    $result1,
+                                    $result2,
+                                ];
+        $expectedResult     =   [ 
+                                    0,
+                                    1,
+                                ];
+
+        $this->assertSame($result,$expectedResult);        
+    }
+
+    /*
+        $result01 =  $this->practice_model->get_average_interval($deck_id = 1 ,$user_id = 1);
+        $result02 =  $this->practice_model->get_average_interval($deck_id = 1 ,$user_id = 0);
+
+        $result         =   [   $result01,
+                                $result02,
+                            ];
+
+        $expectedResult =   [   1,
+                                0,
+
+                            ];
+
+    */
     
 }
