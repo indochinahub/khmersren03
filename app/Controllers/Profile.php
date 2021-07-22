18917c2db2e6db1c5c19bf7aa64f365c95d25b86
indochinahub <indochinahub@gmail.com>
2021-07-22 12:29:23 +0700

    เพิ่ม Profile Controller

---
+++ app/Controllers/Profile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\CourseTypeModel;
+use App\Models\CourseModel;
+use App\Models\UtilModel;
+use App\Models\DeckModel;
+use App\Models\CardModel;
+use App\Models\PracticeModel;
+
+class Profile extends MyController
+{
+
+
+}
+
