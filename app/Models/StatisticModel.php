31976d8d6ac8f91cb0baf3633559ad72d638577a
indochinahub <indochinahub@gmail.com>
2021-07-22 20:37:13 +0700

    StatisticModel:get_sum_spenttime_by_user_id_and_deck_id() เพิ่มพร้อม unittest

--- app/Models/StatisticModel.php
+++ app/Models/StatisticModel.php
@@ -9,6 +9,28 @@ use App\Models\DeckpModel;
 class StatisticModel extends MyModel
 {
 
+    // return int
+    public function get_sum_spenttime_by_user_id_and_deck_id($user_id,$deck_id){
+
+        $sql =  " SELECT sum(statistic_timespent) AS timespent ";
+        $sql .= " FROM statistic ";
+        $sql .= " WHERE id_user = $user_id AND id_deck = $deck_id ";
+        $query = $this->db->query($sql);
+
+        if( $result = $query->getResult() ){
+            return (int) $result[0]->timespent;
+
+       }else{
+           return 0;
+       }
+
+      
+
+
+
+
+    }
+
 
 }
 
--- tests/model/StatisticModelTest.php
+++ tests/model/StatisticModelTest.php
@@ -4,15 +4,47 @@ namespace App\Models;
 
 use CodeIgniter\Test\CIUnitTestCase;
 
-class StatisticModel extends CIUnitTestCase
+class StatisticModelTest extends CIUnitTestCase
 {
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->user_model = new UserModel();
+        $this->statistic_model = new StatisticModel();
 
     }    
+
+    // return int
+    public function test_get_sum_spenttime_by_user_id_and_deck_id(){
+
+        $result1 = $this->statistic_model->get_sum_spenttime_by_user_id_and_deck_id(
+                                                $user_id = 3,
+                                                $deck_id = 18
+                                            );
+        $result2 = $this->statistic_model->get_sum_spenttime_by_user_id_and_deck_id(
+                                                $user_id = 2,
+                                                $deck_id = 18
+                                            );
+        $result3 = $this->statistic_model->get_sum_spenttime_by_user_id_and_deck_id(
+                                                $user_id = 0,
+                                                $deck_id = 0
+                                            );
+    
+        $result         =   [ 
+                                $result1,
+                                $result2,
+                                $result3,
+                            ];
+
+        $expectedResult =   [ 
+                                44,
+                                0,
+                                0,
+                            ];
+
+        $this->assertSame($expectedResult, $result);        
+
+    }
 
     
 }
