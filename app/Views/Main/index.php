9d3d54478d091bad48d5f1f79c0ae7b704189e18
indochinahub <indochinahub@gmail.com>
2021-07-23 19:38:28 +0700

    เพิ่มตัวอย่างของ Accordion

--- app/Views/Main/index.php
+++ app/Views/Main/index.php
@@ -1,164 +1,181 @@
+<div class="box-content">
+    <h3>Collapsible Sidebar Using Bootstrap 4</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="line"></div>
+<div class="box-content">
+    <h3>Collapsible Sidebar Using Bootstrap 4</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
 
+<ul class="list-group">
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        A list item
+        <span class="badge badge-primary badge-pill">14</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        A second list item
+        <span class="badge badge-primary badge-pill">2</span>
+    </li>
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        A third list item
+        <span class="badge badge-primary badge-pill">1</span>
+    </li>
+</ul>
 
+<div class="list-group">
+    <a href="#" class="list-group-item list-group-item-action active">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">List group item heading</h5>
+        <small>3 days ago</small>
+        </div>
+        <p class="mb-1">Some placeholder content in a paragraph.</p>
+        <small>And some small print.</small>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">List group item heading</h5>
+        <small class="text-muted">3 days ago</small>
+        </div>
+        <p class="mb-1">Some placeholder content in a paragraph.</p>
+        <small class="text-muted">And some muted small print.</small>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">List group item heading</h5>
+        <small class="text-muted">3 days ago</small>
+        </div>
+        <p class="mb-1">Some placeholder content in a paragraph.</p>
+        <small class="text-muted">And some muted small print.</small>
+    </a>
+</div>     
 
+<div class="card-success">
+    <div class="card-success card-success_header">This is Headdrr</div>
+    <div class="card-success card-success_body">This is Body</div>
+    <div class="card-success card-success_body">This is Body</div>
+    <div class="card-success card-success_body">This is Body</div>
+</div>
 
-            <div class="box-content">
-                <h3>Collapsible Sidebar Using Bootstrap 4</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
-            <div class="line"></div>
-            <div class="box-content">
-                <h3>Collapsible Sidebar Using Bootstrap 4</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+<div class="card-info">
+    <div class="card-info card-info_header">This is Headdrr</div>
+    <div class="card-info card-info_body">This is Body</div>
+    <div class="card-info card-info_body">This is Body</div>
+    <div class="card-info card-info_body">This is Body</div>
+</div>
 
-            <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    A list item
-                    <span class="badge badge-primary badge-pill">14</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    A second list item
-                    <span class="badge badge-primary badge-pill">2</span>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                    A third list item
-                    <span class="badge badge-primary badge-pill">1</span>
-                </li>
-            </ul>
+<div class="card-warning">
+    <div class="card-warning card-warning_header">This is Headdrr</div>
+    <div class="card-warning card-warning_body">This is Body</div>
+    <div class="card-warning card-warning_body">This is Body</div>
+    <div class="card-warning card-warning_body">This is Body</div>
+</div>
 
-            <div class="list-group">
-                <a href="#" class="list-group-item list-group-item-action active">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small>3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small>And some small print.</small>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small class="text-muted">And some muted small print.</small>
-                </a>
-                <a href="#" class="list-group-item list-group-item-action">
-                    <div class="d-flex w-100 justify-content-between">
-                    <h5 class="mb-1">List group item heading</h5>
-                    <small class="text-muted">3 days ago</small>
-                    </div>
-                    <p class="mb-1">Some placeholder content in a paragraph.</p>
-                    <small class="text-muted">And some muted small print.</small>
-                </a>
-            </div>     
+<div class="card-danger">
+    <div class="card-danger card-danger_header">This is Headdrr</div>
+    <div class="card-danger card-danger_body">This is Body</div>
+    <div class="card-danger card-danger_body">This is Body</div>
+    <div class="card-danger card-danger_body">This is Body</div>
+</div>            
 
-            <div class="card-success">
-                <div class="card-success card-success_header">This is Headdrr</div>
-                <div class="card-success card-success_body">This is Body</div>
-                <div class="card-success card-success_body">This is Body</div>
-                <div class="card-success card-success_body">This is Body</div>
-            </div>
+<div class="card-info">
+    <div class="card-info card-info_header">ภาษาเขมร</div>
+    <div class="card-info card-info_body">
+
+        <div class="icon_row">
             
-            <div class="card-info">
-                <div class="card-info card-info_header">This is Headdrr</div>
-                <div class="card-info card-info_body">This is Body</div>
-                <div class="card-info card-info_body">This is Body</div>
-                <div class="card-info card-info_body">This is Body</div>
-            </div>
-
-            <div class="card-warning">
-                <div class="card-warning card-warning_header">This is Headdrr</div>
-                <div class="card-warning card-warning_body">This is Body</div>
-                <div class="card-warning card-warning_body">This is Body</div>
-                <div class="card-warning card-warning_body">This is Body</div>
-            </div>
-
-            <div class="card-danger">
-                <div class="card-danger card-danger_header">This is Headdrr</div>
-                <div class="card-danger card-danger_body">This is Body</div>
-                <div class="card-danger card-danger_body">This is Body</div>
-                <div class="card-danger card-danger_body">This is Body</div>
-            </div>            
-
-            <div class="card-info">
-                <div class="card-info card-info_header">ภาษาเขมร</div>
-                <div class="card-info card-info_body">
-
-                    <div class="icon_row">
-                        
-                        <div class="course_icon">
-                            <div class="course_icon-thumbnail">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/2">
-                                    <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
-                                </a>
-                            </div>
-                            <div class="course_icon-title" style="">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/2">
-                                    <strong>KH103T</strong><br>
-                                    เรียนภาษาเขมรจากโฆษณา                    </a>                        
-                            </div>		
-                        </div>		
-                        
-                        <div class="course_icon">
-                            <div class="course_icon-thumbnail">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/3">
-                                    <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
-                                </a>
-                            </div>
-                            <div class="course_icon-title" style="">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/3">
-                                    <strong>KH104T</strong><br>
-                                    เรียนภาษาเขมรจากเพลง </a>                        
-                            </div>		
-                        </div>		
-                        
-        
-        
-
-        
-                        
-
-                    </div>
-                    <div class="icon_row">
-                        
-                        <div class="course_icon">
-                            <div class="course_icon-thumbnail">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/2">
-                                    <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
-                                </a>
-                            </div>
-                            <div class="course_icon-title" style="">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/2">
-                                    <strong>KH103T</strong><br>
-                                    เรียนภาษาเขมรจากโฆษณา                    </a>                        
-                            </div>		
-                        </div>		
-                        
-                        <div class="course_icon">
-                            <div class="course_icon-thumbnail">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/3">
-                                    <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
-                                </a>
-                            </div>
-                            <div class="course_icon-title" style="">
-                                <a href="http://127.0.0.1/khmersren02/Course/show/3">
-                                    <strong>KH104T</strong><br>
-                                    เรียนภาษาเขมรจากเพลง </a>                        
-                            </div>		
-                        </div>		
-                        
-        
-        
-
-        
-                        
-
-                    </div>
-
-
+            <div class="course_icon">
+                <div class="course_icon-thumbnail">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/2">
+                        <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
+                    </a>
                 </div>
-            </div>
+                <div class="course_icon-title" style="">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/2">
+                        <strong>KH103T</strong><br>
+                        เรียนภาษาเขมรจากโฆษณา                    </a>                        
+                </div>		
+            </div>		
+            
+            <div class="course_icon">
+                <div class="course_icon-thumbnail">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/3">
+                        <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
+                    </a>
+                </div>
+                <div class="course_icon-title" style="">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/3">
+                        <strong>KH104T</strong><br>
+                        เรียนภาษาเขมรจากเพลง </a>                        
+                </div>		
+            </div>		
+            
+
+
+
+
+            
+
+        </div>
+        <div class="icon_row">
+            
+            <div class="course_icon">
+                <div class="course_icon-thumbnail">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/2">
+                        <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
+                    </a>
+                </div>
+                <div class="course_icon-title" style="">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/2">
+                        <strong>KH103T</strong><br>
+                        เรียนภาษาเขมรจากโฆษณา                    </a>                        
+                </div>		
+            </div>		
+            
+            <div class="course_icon">
+                <div class="course_icon-thumbnail">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/3">
+                        <img src="http://127.0.0.1/khmersren02/assets/course/course_thumbnail.jpg" width="100%">
+                    </a>
+                </div>
+                <div class="course_icon-title" style="">
+                    <a href="http://127.0.0.1/khmersren02/Course/show/3">
+                        <strong>KH104T</strong><br>
+                        เรียนภาษาเขมรจากเพลง </a>                        
+                </div>		
+            </div>		
+            
+
+
+
+
+            
+
+        </div>
+
+
+    </div>
+</div>
+
+<div class="accordion" id="accordionExample">
+  <div class="card-danger">
+    <div class="card-danger card-danger_header" id="headingOne">
+      <h2 class="mb-0">
+        <button class="btn btn-block text-center" type="button" data-toggle="collapse" 
+           data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+           style="color:white;font-size:1rem"
+        >
+            Collapsible Group Item #1 
+        </button>
+      </h2>
+    </div>
+
+    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+      <div class="card-danger card-danger_body">
+        Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
+      </div>
+    </div>
+  </div>
+</div>
