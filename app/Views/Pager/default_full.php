4bd1cae0260baa4a4d5be318e0050373fa4d6c45
indochinahub <indochinahub@gmail.com>
2021-07-24 20:43:23 +0700

    ปรับปรุง default template ของ pagination

--- app/Views/Pager/default_full.php
+++ app/Views/Pager/default_full.php
@@ -7,32 +7,43 @@
 $pager->setSurroundCount(1);
 ?>
 
-<nav aria-label="<?= lang('Pager.pageNavigation') ?>">
-	<ul class="pagination">
-		<?php if ($pager->hasPrevious()) : ?>
+<div class="two_flex_column" style="margin-top:10px;margin-bottom:10px">
+    <div>
+	</div>
 
-			<li>
-				<a href="<?= $pager->getPrevious() ?>" aria-label="<?= lang('Pager.previous') ?>">
-					<span aria-hidden="true"><?= lang('Pager.previous') ?></span>
-				</a>
-			</li>
-		<?php endif ?>
+    <div>
+		<nav aria-label="<?= lang('Pager.pageNavigation') ?>">
+			<ul class="pagination" style="margin:0px">
 
-		<?php foreach ($pager->links() as $link) : ?>
-			<li <?= $link['active'] ? 'class="active"' : '' ?>>
-				<a href="<?= $link['uri'] ?>">
-					<?= $link['title'] ?>
-				</a>
-			</li>
-		<?php endforeach ?>
+				<?php if ($pager->hasPrevious()) : ?>
+					<li class="page-item">
+						<a class="page-link" href="<?= $pager->getPrevious() ?>" tabindex="-1" aria-disabled="true">
+							<?= lang('Pager.previous') ?>
+						</a>
+					</li>
+				<?php endif ?>
 
-		<?php if ($pager->hasNext()) : ?>
-			<li>
-				<a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
-					<span aria-hidden="true"><?= lang('Pager.next') ?></span>
-				</a>
-			</li>
+				<?php foreach ($pager->links() as $link) : ?>
+					<li <?php if($link['active']){ echo 'class="page-item active"';}else{ echo 'class="page-item"';} ?> >
+						<a class="page-link" href="<?= $link['uri'] ?>"><?= $link['title'] ?></a>
+					</li>
+				<?php endforeach ?>
 
-		<?php endif ?>
-	</ul>
-</nav>
+				<?php if ($pager->hasNext()) : ?>
+					<li class="page-item">
+						<a class="page-link" href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>">
+							<?= lang('Pager.next') ?>
+						</a>
+					</li>
+				<?php endif ?>
+
+			</ul>
+		</nav>
+	</div>
+</div>
+
+
+
+
+	
+
