bcba76def354d470041c6abb92c7878d66b11df7
indochinahub <indochinahub@gmail.com>
2021-08-11 14:57:58 +0700

    แสดงโพสท์ล่าสุด 3 โพสท์ในหน้าแรก

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -2,8 +2,20 @@
 
 namespace App\Controllers;
 
-use App\Models\UserModel;
+use App\Models\CourseTypeModel;
+use App\Models\CourseModel;
+use App\Models\UtilModel;
+use App\Models\DeckModel;
+use App\Models\CardModel;
+use App\Models\PracticeModel;
 use App\Models\PaginationModel;
+use App\Models\CardcommentModel;
+use App\Models\UserModel;
+use App\Models\PostModel;
+use App\Models\MediaModel;
+use App\Models\DateTimeModel;
+use App\Models\PostcategoryModel;
+
 
 class Main extends MyController
 {
@@ -11,9 +23,42 @@ class Main extends MyController
 
 	public function index(){
 
-		$data["page_title"] = 	"Title "; 
-		$data["page_link"] 	= 	[   "กลับ",
-									$this->_get_backlink()
+        $post_model         = new PostModel;
+        $util_model         = new UtilModel;
+        $pagination_model   = new PaginationModel;
+        $datetime_model     = new DateTimeModel;
+        $user_model         = new UserModel;
+        $postcategory_model = new PostcategoryModel;
+
+		// Post Section
+        $arr_post = $post_model->get_all_row();
+        $arr_post = $util_model->sort_array_of_object_by_the_property( 
+						$arr_post, 
+						$sorted_property = "post_id", 
+						$order_by ="desc"
+					);
+		$arr_post = array_slice( $arr_post, 0, 3);
+
+        $data["arr_post"] = [];
+        foreach( $arr_post as $post){
+
+            $media_model        = new MediaModel( $post, "post");
+            $post->post_intro   = $media_model->replace_media_tag_with_html($post->post_intro);
+
+            $post->user = $user_model->get_by_post_id( $post->post_id );
+            $post->post_createddate = $datetime_model->get_thai_datetime_from_sql_timestamp(
+                                        $post->post_createddate );
+            $post->postcategory = $postcategory_model->get_by_post_id($post->post_id);
+            $post->postcategory_num_card = $post_model->get_num_by_postcategory_id( $post->id_postcategory);
+
+            //$post = $post_model->add_media_to_post($post);
+            array_push( $data["arr_post"], $post);
+
+        }
+
+		$data["page_title"] = 	" "; 
+		$data["page_link"] 	= 	[   "",
+									""
 							   ];
 		$this->_view("index",$data);             
 
--- app/Views/Main/index.php
+++ app/Views/Main/index.php
@@ -1,1 +1,66 @@
-xxxxx
+<div class="card-info">
+    <div class="card-info card-info_body">
+        <h2>สวัสดีผู้เยี่ยมชมทุกท่าน</h2>
+        ยินดีต้อนรับทุกท่านเข้าสู่เว็บไซต์ www.khmersren.com  เว็บไซต์ที่ส่งเสริมการเรียนรู้สิ่งต่างๆ ด้วยตนเอง <br>
+        <br>
+        สำหรับท่านที่เป็นสมาชิกอยู่แล้ว ท่านสามารถ<strong>เข้าสู่ระบบ</strong> ได้  <a href="http://www.khmersren.com/User/login">[ โดยคลิ๊กที่ลิงก์นี้ ]</a><br>  หรือหากท่านสนใจต้องการใช้งานก็สามารถสมัครขอใช้งานฟรีได้ที่โดยส่งข้อความมาที่ [<a href="https://www.facebook.com/khmersren01">Facebook Fanpage :: Khmersren.com  </a>]        
+    </div>
+</div>
+
+<div class="card-info">
+    <div class="card-info card-info_header">บทความล่าสุด</div>
+
+    <?php foreach( $arr_post as $post ){ ?>
+
+        <div class="card-info card-info_body">
+
+            <div>
+                <h5><a href="<?php echo base_url([ "Post","show",$post->post_id ]);?>"><?php echo $post->post_title;?></a></h5>
+                <h6>
+                    [ <?php echo $post->post_createddate;?>
+                        โดย 
+                        <a href="<?php echo base_url( ["User","myProfile", $post->user->user_id]);?>">
+                            <?php echo $post->user->displayname;?>
+                        </a>]
+                </h6>
+            </div>
+
+            <div style="margin-bottom:15px">
+                <?php echo $post->post_intro;?>
+                    
+            </div>
+
+            <div class="two_flex_column">
+                <div>
+                    <a href="<?php echo base_url( ["Post","showBy","Category",$post->postcategory->postcategory_id] );?>">
+                        <strong>#<?php echo $post->postcategory->postcategory_title;?></strong>
+                    </a>
+                    [ <?php echo $post->postcategory_num_card;?> ]
+                </div>
+                <div>
+                    <a href="<?php echo base_url([ "Post","show",$post->post_id ]);?>" class="btn btn-primary">อ่าน</a>
+                </div>
+            </div>
+            
+        </div>
+
+    <?php } ?>
+
+</div>
+
+<div class="card-warning">
+
+    <div class="card-warning card-warning_body">
+        
+        <div class="two_flex_column">
+            <div>
+                <h5>ดูบัตรคำทั้งหมด</h5>
+            </div>
+            <div>
+                <a href="<?php echo base_url( ["Post","showBy","All"] );?>" class="btn btn-primary">ไป</a>
+            </div>
+        </div>
+
+    </div>
+
+</div>
