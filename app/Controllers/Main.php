7ec664a805c57d62182812ea084523ba1671b583
indochinahub <indochinahub@gmail.com>
2021-08-11 14:38:08 +0700

    ปรับการแสดงผล MainController:index

--- app/Controllers/Main.php
+++ app/Controllers/Main.php
@@ -11,54 +11,12 @@ class Main extends MyController
 
 	public function index(){
 
-	/******************************* */
-	// Pagination
-	/******************************* */
-
-	$pager = service("pager");
-	$perPage = 5;
-	$total = 33;
-	$data["pagination"]  = $pager->makeLinks( $page = 3 , $perPage , $total);
-
-	$this->_view("index",$data);
-
-
-
-
-
+		$data["page_title"] = 	"Title "; 
+		$data["page_link"] 	= 	[   "กลับ",
+									$this->_get_backlink()
+							   ];
+		$this->_view("index",$data);             
 
 	}
-
-
-	public function PaginationExample()	{
-
-	/******************************* */
-	// Pagination
-	/******************************* */
-
-		$pager = service("pager");
-		$perPage = 5;
-		$total = 33;
-		$data["pagination"]  = $pager->makeLinks( $page = 3 , $perPage , $total);
-
-		$this->_view("index",$data);
-
-	}
-
-	public function confirm(){
-		
-		$data    =  [   "page_title"=>"ยืนยันการชุดบัตรคำ",
-						"what_happened"=>"นี่คือสิ่งที่คุณได้ดำเนินการ ",
-						"what_todo" => "คลิ๊กที่ปุ่ม \"<strong>ยืนยัน</strong>\" หรือปุ่ม \"<strong>ยกเลิก</strong>\" ",
-						"btnText_toConfirm" => "ยืนยัน",
-						"btnLink_toConfirm" => base_url(),
-						"btnText_toCancle" => "ยกเลิก",
-						"btnLink_toCancle" => base_url(),
-					];  		
-		$this->_confirm($data);
-
-	}
-
-
 }
 
