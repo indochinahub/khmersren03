b483c7ae186972fe465cc9b369c14a0e996b77de
indochinahub <indochinahub@gmail.com>
2021-08-18 19:06:28 +0700

    ปรับการแสดงผล

--- app/Views/Message/with.php
+++ app/Views/Message/with.php
@@ -5,9 +5,8 @@
     <?php if( $message->role === "i_am_reciever" ){ ?>
 
         <div class="card-info" style="margin-right:40px;margin-bottom:5px"">
-            <div class="card-info card-info_body">
-                <div class="two_flex_column">
-                    <div style="text-align:left">
+            <div class="card-info card-info_body" style="text-align:left">
+
                         <div>
                             <?php echo $message->message_text;?>
                         </div>
@@ -15,10 +14,7 @@
                             โดย [<strong><?php echo $other_displayname;?></strong>]<br>
                             ส่งเมื่อ <?php echo $message->message_senddate;?>
                         </div>
-                    </div>
-                    <div>
-                    </div>
-                </div>
+
             </div>
         </div>
 
@@ -85,22 +81,15 @@
     <?php }elseif(   $message->role === "i_am_sender"    ){ ?>
 
         <div class="card-warning" style="margin-left:40px;margin-bottom:5px">
-            <div class="card-warning card-warning_body">
+            <div class="card-warning card-warning_body" style="text-align:right">
 
-                <div class="two_flex_column">
-                    <div>
-                    </div>            
-                    <div style="text-align:right">
-                        <div>
-                            <?php echo $message->message_text;?>
-                        </div>
-                        <div>
-                            โดย [<strong><?php echo $user_displayname;?></strong>]<br>
-                            <?php echo $message->message_readdate;?>
-                        </div>
-                    </div>
+                <div>
+                    <?php echo $message->message_text;?>
                 </div>
-
+                <div>
+                    โดย [<strong><?php echo $user_displayname;?></strong>]<br>
+                    <?php echo $message->message_readdate;?>
+                </div>
             </div>
         </div>
 
