236e35d55c79ae8ce0c182ea4c3e48b4fd73b34b
indochinahub <indochinahub@gmail.com>
2021-08-19 18:55:15 +0700

    dashboar เพิมลิงก์ไปยังส่วนจัดการ cardgroup และ table

---
+++ app/Views/Admin/dashboard.php
@@ -0,0 +1,28 @@
+<div class="card-info">
+
+    <div class="card-info card-info_body">
+
+        <div class="two_flex_column" style="margin-bottom:5px;">
+            <div>
+                <h5>จัดการกลุ่มบัตรคำ</h5>
+            </div>
+            <div>
+                <a href="<?php echo base_url(["Admin","manageCardgroup"]);?>" class="btn btn-primary">Export</a>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="card-info card-info_body">    
+            
+        <div class="two_flex_column" style="margin-bottom:5px;">
+            <div>
+                <h5>จัดการตาราง</h5>
+            </div>
+            <div>
+                <a href="<?php echo base_url(["Admin","manageTable"]);?>" class="btn btn-primary">Export</a>
+            </div>
+        </div>        
+    </div>
+    
+</div>
