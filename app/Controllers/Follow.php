1ce64be0c8d29b6f4ab7ccdac4900957be805c50
indochinahub <indochinahub@gmail.com>
2021-08-21 19:12:46 +0700

    Follow:myFollow() เพิ่มส่วนแสดง ผู้ที่ติดตามฉัน

--- app/Controllers/Follow.php
+++ app/Controllers/Follow.php
@@ -49,12 +49,15 @@ class Follow extends MyController
 
         if( $data["user"]->user_id === $data["member"]->user_id ){
             $data["if_user_view_own_profile"] = true;
+            $data["display_name"] = "ฉัน";
+
         }else{
             $data["if_user_view_own_profile"] = false;
+            $data["display_name"] = $user_model->get_user_displayname($data["member"]);
         }
 
+        // Who I follow
         $arr_id_of_whom_i_follow = $follow_model->get_id_of_whom_i_follow($member_id);
-
 
         $data["arr_of_whom_i_follow"] = [];
         foreach( $arr_id_of_whom_i_follow as $id ){
@@ -71,65 +74,41 @@ class Follow extends MyController
                                             );
 
         $num_row = (int) ceil( count($data["arr_of_whom_i_follow"])/4) ;
-        //var_dump( $num_row);
-        //die();
         $data["arr_of_whom_i_follow"] = $util_model->saparate_array_to_row(
                                                 $data["arr_of_whom_i_follow"],
                                                 $num_row,
                                                 4
                                             );
 
-        
+        // Who follow me
+        $arr_id_of_my_follower = $follow_model->get_my_follower_id($member_id);
 
-
-        /*
-        $arr_id_whom_i_relate_to = $follow_model->get_id_of_whom_i_relate_to($data["member"]->user_id);
-        $arr_id_whom_i_not_relate_to = $follow_model->get_id_of_whom_i_not_relate_to($data["member"]->user_id,8);
-
-        $arr_id_of_user_to_show = array_values(array_merge( $arr_id_whom_i_relate_to,$arr_id_whom_i_not_relate_to ));
-        $arr_id_of_user_to_show = array_slice( $arr_id_of_user_to_show, 0, 8);
-
-        $data["arr_user_to_show"] = [];
-        foreach( $arr_id_of_user_to_show as $id ){
+        $data["arr_of_my_follower"] = [];
+        foreach( $arr_id_of_my_follower as $id ){
             $user = $user_model->get_by_id($id);
             $user->displayname = $user_model->get_user_displayname($user);
             $user->avarta_url = $user_model->get_avarta_url($user->user_id);
-            array_push( $data["arr_user_to_show"], $user);
+            array_push( $data["arr_of_my_follower"], $user);
         }
 
-        $data["arr_user_to_show"] = $util_model->sort_array_of_object_by_the_property( 
-                                        $data["arr_user_to_show"], 
-                                        "user_visit_time", 
-                                        $order_by ="desc"
-                                    );
+        $data["arr_of_my_follower"] = $util_model->sort_array_of_object_by_the_property( 
+                                                $data["arr_of_my_follower"], 
+                                                "user_visit_time", 
+                                                $order_by ="desc"
+                                            );
 
-        $data["arr_user_to_show"] = $util_model->saparate_array_to_row(
-                                        $data["arr_user_to_show"],
-                                        2,
-                                        4
-                                    );
+        $num_row = (int) ceil( count($data["arr_of_my_follower"])/4) ;
+        $data["arr_of_my_follower"] = $util_model->saparate_array_to_row(
+                                                $data["arr_of_my_follower"],
+                                                $num_row,
+                                                4
+                                            );
 
-
-        */
-
-
-
-
-
-
-
-
-
-
-
-
-
-        $data["page_title"] = 	" xxx และผู้ที่เกี่ยวข้อง ";
+        $data["page_title"] = 	$data["display_name"]." และผู้ที่เกี่ยวข้อง ";
         $data["page_link"] 	= 	[   "หน้าที่แล้ว",
                                     $this->_get_backlink()
                                 ];
         $this->_view("myFollow",$data);        
-        
     }
 
 
--- app/Views/Follow/myFollow.php
+++ app/Views/Follow/myFollow.php
@@ -1,7 +1,10 @@
 <div class="card-info">
     <div class="card-info card-info_header">
-        ผู้ที่ฉันติดตาม
+        ผู้ที่ <?php echo $display_name;?> ติดตาม
+        
+
     </div>
+
     <div class="card-info card-info_body">
 
         <?php foreach( $arr_of_whom_i_follow as $row_of_user ){ ?>
@@ -33,6 +36,36 @@
     </div>
     
     <div class="card-info card-info_header">
-        ผู้ติดตามฉัน
+        ผู้ติดตาม <?php echo $display_name;?> 
     </div>
+
+    <div class="card-info card-info_body">
+
+        <?php foreach( $arr_of_my_follower as $row_of_user ){ ?>
+            
+            <div style="display:flex;justify-content:space-evenly">
+                                                
+                <?php foreach( $row_of_user as $user ){ ?>
+                    
+                    <?php if($user ){ ?>
+                        <div style="background-color:#becae6;width:24%">
+                                <a href="<?php echo base_url(["User","myProfile",$user->user_id]);?>"> 
+                                    <img style="border-radius:5%;border-style:solid;border-width:2px;border-color:black;" class="card-img-top" 
+                                    src="<?php echo $user->avarta_url;?>">
+                                </a><br>
+                                <?php echo $user->displayname;?>
+                                
+                        </div>
+                    <?php }else{ ?>
+                        <div style="width:24%">
+                        </div>
+                    <?php } ?>
+
+
+                <?php } ?>
+            </div>
+
+        <?php } ?>      
+    
+    </div>    
 </div>
