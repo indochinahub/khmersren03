f41d440ea104e19a9752ae31a63f96b17150380f
indochinahub <indochinahub@gmail.com>
2021-08-31 18:21:12 +0700

    ปรับปรุง css

--- app/Views/User/login.php
+++ app/Views/User/login.php
@@ -1,22 +1,26 @@
-<div class="box-content">
-    
-    <form action="<?php echo base_url(["User","login"]);?>" method="post">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" name="username" id="username" value="">
-            <?php if( isset($username_error) && $username_error != "" ){ ?>
-                <div class="form-error">[<?php echo $username_error;?>]</div>
-            <?php } ?>
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="text" class="form-control" name="password" id="password" maxlength="4" minlength="4">
-            <?php if( isset($password_error) && $password_error != "" ){ ?>
-                <div class="form-error">[<?php echo $password_error;?>]</div>
-            <?php } ?>
-        </div>
-        <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-    </form>
+<div class="box">
+
+    <div class="box__body box__body--info">
+
+        <form action="<?php echo base_url(["User","login"]);?>" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" id="username" value="">
+                <?php if( isset($username_error) && $username_error != "" ){ ?>
+                    <div class="form-error">[<?php echo $username_error;?>]</div>
+                <?php } ?>
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="text" class="form-control" name="password" id="password" maxlength="4" minlength="4">
+                <?php if( isset($password_error) && $password_error != "" ){ ?>
+                    <div class="form-error">[<?php echo $password_error;?>]</div>
+                <?php } ?>
+            </div>
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+    </div>
 
 </div>
 
